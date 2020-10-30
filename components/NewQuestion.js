cdc2ee2351c95259df12d1b81ed69ee74a4c4b89
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-10-29 21:53:08 -0400

    chore: connect NewQuestion screen

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Card } from 'react-native-elements';
+import { connect } from 'react-redux';
+
+import { handleAddQuestion } from '../actions/questions';
 
 class NewQuestion extends Component {
+  state = {
+    optionOneText: '',
+    optionTwoText: '',
+  };
+
+  handlePress = () => {
+    const { optionOneText, optionTwoText } = this.state;
+    const { dispatch } = this.props;
+
+    dispatch(handleAddQuestion(optionOneText, optionTwoText));
+    this.setState(() => ({
+      optionOneText: '',
+      optionTwoText: '',
+    }));
+
+    this.props.navigation.push('Home', { userName: this.props.authedUser });
+  };
+
   render() {
     return (
-      <View>
-        <Text>NewQuestion</Text>
-      </View>
+      <SafeAreaView>
+        <Card>
+          <Card.Title>Create New Question</Card.Title>
+          <Card.Divider />
+          <Text>Complete the question:</Text>
+          <Text>Would you rather...</Text>
+          <TextInput
+            onChangeText={(text) => this.setState({ optionOneText: text })}
+            value={this.state.optionOneText}
+            placeholder='Enter option one here'
+          />
+          <TextInput
+            onChangeText={(text) => this.setState({ optionTwoText: text })}
+            value={this.state.optionTwoText}
+            placeholder='Enter option two here'
+          />
+          <TouchableOpacity delayPressIn={0} onPress={() => this.handlePress()}>
+            <Text>SUBMIT</Text>
+          </TouchableOpacity>
+        </Card>
+      </SafeAreaView>
     );
   }
 }
 
-export default NewQuestion;
+function mapStateToProps(state) {
+  return {
+    authedUser: state.authedUser,
+  };
+}
+
+export default connect()(NewQuestion);
