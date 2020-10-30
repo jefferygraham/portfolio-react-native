58a5f66a98140b909129db7fdcbb45f39cdf2486
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-10-30 09:40:22 -0400

    chore: connect PollResults screen

--- components/PollResults.js
+++ components/PollResults.js
@@ -1,14 +1,64 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, SafeAreaView, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { FontAwesome } from '@expo/vector-icons';
 
 class PollResults extends Component {
   render() {
+    const { questionId } = this.props.route.params;
+    const { users, questions, authedUser } = this.props;
+    const question = questions[questionId];
+    const optionOneVotes = question.optionOne.votes.length;
+    const optionTwoVotes = question.optionTwo.votes.length;
+    const totalVotes = optionOneVotes + optionTwoVotes;
+    const optionOnePercentage = (optionOneVotes / totalVotes) * 100;
+    const optionTwoPercentage = (optionTwoVotes / totalVotes) * 100;
+
     return (
-      <View>
-        <Text>PollResults</Text>
-      </View>
+      <SafeAreaView>
+        <Card>
+          <Card.Title>
+            {users[questions[questionId].author].name} asked would you rather:
+            <View>
+              <View>
+                <Text>
+                  {question.optionOne.text}{' '}
+                  {users[authedUser].answers[questionId] === 'optionOne' ? (
+                    <FontAwesome name='check-square' />
+                  ) : null}
+                </Text>
+              </View>
+              <Text>
+                {optionOneVotes} out of {totalVotes} votes
+              </Text>
+            </View>
+            <View>
+              <View>
+                <Text>
+                  {question.optionTwo.text}{' '}
+                  {users[authedUser].answers[questionId] === 'optionTwo' ? (
+                    <FontAwesome name='check-square' />
+                  ) : null}
+                </Text>
+              </View>
+              <Text>
+                {optionTwoVotes} out of {totalVotes} votes
+              </Text>
+            </View>
+          </Card.Title>
+        </Card>
+      </SafeAreaView>
     );
   }
 }
 
-export default PollResults;
+function mapStateToProps(state) {
+  return {
+    authedUser: state.authedUser,
+    questions: state.questions,
+    users: state.users,
+  };
+}
+
+export default connect(mapStateToProps)(PollResults);
